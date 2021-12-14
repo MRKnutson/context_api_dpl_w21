5fe3e2852c983baa09dd1b8e85ceaea6d98a3956
Sky Lin <linsky519@gmail.com>
2021-12-14 16:35:13 -0700

    Home page updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,94 @@
-import React from 'react'
-import { Container } from 'semantic-ui-react';
+// import React from 'react'
+// import { Container } from 'semantic-ui-react';
 
-const Home = () => {
-  return(
-    <Container>
-      <h1>Welcome to the Party!</h1>
-    </Container>
-  )
-};
+// const Home = () => {
+//   return(
+//     <Container>
+//       <h1>Welcome to the Party!</h1>
+//     </Container>
+//   )
+// };
+
+// export default Home;
+
+import React, { Component } from 'react'
+import { Container, Form, Grid, Image, Transition } from 'semantic-ui-react'
+
+// const Home = () => {
+  const transitions = [
+    'jiggle',
+    'flash',
+    'shake',
+    'pulse',
+    'tada',
+    'bounce',
+    'glow',
+  ]
+
+  const options = transitions.map((name) => ({
+    key: name,
+    text: name,
+    value: name,
+  }))
+
+  class Home extends Component {
+    state = { animation: transitions[0], duration: 500, visible: true }
+
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+    toggleVisibility = () =>
+      this.setState((prevState) => ({ visible: !prevState.visible }))
+
+    render() {
+      const { animation, duration, visible } = this.state
+
+      return (
+        <Container>
+          {/* <h1>Welcome to the Partyyyyy!!!</h1> */}
+        <Grid columns={1}>
+          <Grid.Column>
+            <Transition
+              animation={animation}
+              duration={duration}
+              visible={visible}
+            >
+              <h1>Welcome to the Partyyyyy!!!</h1>
+            </Transition>
+            <Transition
+              animation={animation}
+              duration={duration}
+              visible={visible}
+            >
+              <Image centered size='small' src='https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/77351/confetti-clipart-xl.png' />
+            </Transition>
+          </Grid.Column>
+          <br />
+          <Grid.Column as={Form}>
+            <Form.Select
+              label='Choose transition'
+              name='animation'
+              onChange={this.handleChange}
+              options={options}
+              value={animation}
+            />
+            <Form.Input
+              label={`Duration: ${duration}ms `}
+              min={100}
+              max={2000}
+              name='duration'
+              onChange={this.handleChange}
+              step={100}
+              type='range'
+              value={duration}
+            />
+            <Form.Button content='Run' onClick={this.toggleVisibility} />
+          </Grid.Column>
+
+          
+        </Grid>
+        </Container>
+      )
+    }
+  }
+
 
 export default Home;
